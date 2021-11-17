e01d73578b3cd3df268c2cf33e21625b38a91a6b
David <69880497+Brozoka@users.noreply.github.com>
2021-11-17 09:04:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
-# import the Pandas library
 import pandas as pd
+from azure.storage.blob import BlobServiceClient
+from azure.storage.filedatalake import DataLakeServiceClient
+import azure.functions as func
+from io import StringIO
 
-# extract
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    STORAGEACCOUNTURL= 'https://{storage account name}.blob.core.windows.net/'
+    STORAGEACCOUNTKEY= '****'
+    LOCALFILENAME= ['file1.csv', 'file2.csv']
+    CONTAINERNAME= 'name of the container'
 
-# load the csv data into the 2 dataframes
-table_original = pd.read_csv(r'file_path', encoding='utf-8')
-table_original_2 = pd.read_csv(r'file_path', encoding='utf-8')
+    file1 = pd.DataFrame()
+    file2 = pd.DataFrame()
+    #download from blob
 
-# creating an empty dataframe for the final data
-table = pd.DataFrame(columns=[''])
+    blob_service_client_instance = BlobServiceClient(account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
+    for i in LOCALFILENAME:
+            blob_client_instance = blob_service_client_instance.get_blob_client(container=CONTAINERNAME, blob=i, snapshot=None)
+            blob_data = blob_client_instance.download_blob()
+            if i == 'file1.csv':
+                file1 = pd.read_csv(StringIO(blob_data.content_as_text()))
+            if i == 'file2.csv':
+                file2 = pd.read_csv(StringIO(blob_data.content_as_text()))
+    
+    # replace all null values with either 0 or a string, also do type casting for strings
+    file1['column name'] = file1['column name'].fillna(0)
 
-# transform
+    # replace all null values with either 0 or a string, also do type casting for strings
+    file2['column name'] = breweries['column name'].fillna(int(0))
+    
+    # load
+    summary = pd.merge(left=file1, right=file2, on='key column name', how='inner')
+    summary.to_csv()
 
-# replace all null values with either 0 or a string, also do type casting for strings
-table['column_name'] = table_original['column_name'].fillna(0)
+    service_client = DataLakeServiceClient(account_url="https://{storage account name}.dfs.core.windows.net/", credential=STORAGEACCOUNTKEY)
+    file_system_client = service_client.get_file_system_client(file_system="{container name}")
+    directory_client = file_system_client.get_directory_client("{folder name}") 
+    file_client = directory_client.create_file("{file name}.csv") 
+    file_contents = summary.to_csv()
+    file_client.upload_data(file_contents, overwrite=True) 
 
-# replace all null values with either 0 or a string, also do type casting for strings
-table_original_2['column_name'] = table_original_2['column_name'].fillna(int(0))
+    return("This HTTP triggered function executed successfully.")
 
-# load
-
-# join the 2 dataframes into the final dataframe on the key column
-summary = pd.merge(left=table, right=table_original_2, on='key_column_name', how='inner')
-
-# load the data into a csv file and save it to a local folder
-summary.to_csv(path_or_buf=r'save_path', index=False, encoding='utf-8')
+if __name__ == '__main__':
+    main("name")
