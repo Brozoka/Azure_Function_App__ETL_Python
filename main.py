211b02ae12b5fcefdcc8a4381cee675e1e2d2b58
David <69880497+Brozoka@users.noreply.github.com>
2021-12-08 14:59:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,68 @@
 import pandas as pd
-from azure.storage.blob import BlobServiceClient
 from azure.storage.filedatalake import DataLakeServiceClient
 import azure.functions as func
 from io import StringIO
+import yaml
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    STORAGEACCOUNTURL= 'https://{storage account name}.blob.core.windows.net/'
-    STORAGEACCOUNTKEY= '****'
-    LOCALFILENAME= ['file1.csv', 'file2.csv']
-    CONTAINERNAME= 'name of the container'
+    
+    credentials = yaml.load(open('./credentials.yml'), Loader=yaml.FullLoader)
+    
+    STORAGEACCOUNTURL= credentials['STORAGEACCOUNTURL']
+    STORAGEACCOUNTKEY= credentials['STORAGEACCOUNTKEY']
+    LOCALFILENAME= ['beers.csv', 'breweries.csv']
 
-    file1 = pd.DataFrame()
-    file2 = pd.DataFrame()
-    #download from blob
+    beer = pd.DataFrame()
+    breweries = pd.DataFrame()
+    
+    #download the data from adls
+    service_client = DataLakeServiceClient(account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
+    adl_client_instance = service_client.get_file_system_client(file_system="raw")
+    directory_client = adl_client_instance.get_directory_client("raw")
 
-    blob_service_client_instance = BlobServiceClient(account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
-    for i in LOCALFILENAME:
-            blob_client_instance = blob_service_client_instance.get_blob_client(container=CONTAINERNAME, blob=i, snapshot=None)
-            blob_data = blob_client_instance.download_blob()
-            if i == 'file1.csv':
-                file1 = pd.read_csv(StringIO(blob_data.content_as_text()))
-            if i == 'file2.csv':
-                file2 = pd.read_csv(StringIO(blob_data.content_as_text()))
+    #load the data into dataframes
+    file_client = adl_client_instance.get_file_client(LOCALFILENAME[0])
+    adl_data = file_client.download_file()
+    beerbyte = adl_data.readall()
+    s=str(beerbyte,'utf-8')
+    beer = pd.read_csv(StringIO(s))
+            
+    file_client = adl_client_instance.get_file_client(LOCALFILENAME[1])
+    adl_data = file_client.download_file()
+    brewriesbyte = adl_data.readall()
+    s=str(brewriesbyte,'utf-8')
+    breweries = pd.read_csv(StringIO(s))
     
     # replace all null values with either 0 or a string, also do type casting for strings
-    file1['column name'] = file1['column name'].fillna(0)
+    beer['abv'] = beer['abv'].fillna(0)
+    beer['ibu'] = beer['ibu'].fillna(0)
+    beer['id'] = beer['id'].fillna(int(0))
+    beer['name_beer'] = beer['name'].astype(str).fillna('No name')
+    beer['style'] = beer['style'].astype(str).fillna('No style')
+    beer['brewery_id'] = beer['brewery_id'].fillna(int(0))
+    beer['ounces'] = beer['ounces'].fillna(0)
 
     # replace all null values with either 0 or a string, also do type casting for strings
-    file2['column name'] = file2['column name'].fillna(int(0))
-    
-    # load
-    summary = pd.merge(left=file1, right=file2, on='key column name', how='inner')
-    summary.to_csv()
+    breweries['brewery_id'] = breweries['brewery_id'].fillna(int(0))
+    breweries['name'] = breweries['name'].astype(str).fillna('No name')
+    breweries['city'] = breweries['city'].astype(str).fillna('No city')
+    breweries['state'] = breweries['state'].astype(str).fillna('No state')
 
-    service_client = DataLakeServiceClient(account_url="https://{storage account name}.dfs.core.windows.net/", credential=STORAGEACCOUNTKEY)
-    file_system_client = service_client.get_file_system_client(file_system="{container name}")
-    directory_client = file_system_client.get_directory_client("{folder name}") 
-    file_client = directory_client.create_file("{file name}.csv") 
-    file_contents = summary.to_csv()
-    file_client.upload_data(file_contents, overwrite=True) 
+    # renaming the column to avoid duplicate columns names
+    breweries = breweries.rename(columns={'name': 'name_breweries'})
+    
+    # merge the 2 csv files
+    summary = pd.merge(left=beer, right=breweries, on='brewery_id', how='inner')
+    
+    #upload the file to adls
+    service_client = DataLakeServiceClient(account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
+    file_system_client = service_client.get_file_system_client(file_system="output")
+    directory_client = file_system_client.get_directory_client("output") 
+    file_client = directory_client.create_file("output.parquet") 
+    file_contents = summary.to_parquet()
+    file_client.append_data(data=file_contents, offset=0, length=len(file_contents)) 
+    
+    file_client.flush_data(len(file_contents))
 
     return("This HTTP triggered function executed successfully.")
 
